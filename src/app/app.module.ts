ec86580bcc16f68780c5f51ce9dcdb90e1f2d716
mduhovniy <maxim.duhovniy@gmail.com>
2017-09-28 20:23:15 +0300

    Add Angular Material package

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,19 @@
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {MATERIAL_COMPATIBILITY_MODE} from "@angular/material";
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MatToolbarModule
   ],
-  providers: [],
+  providers: [{provide: MATERIAL_COMPATIBILITY_MODE, useValue: true}],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
